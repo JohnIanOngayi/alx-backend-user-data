bac20eebf5a55a87dc2a45eb2e386ed575ccae21
JohnIanOngayi <ongayijohnian@gmail.com>
2024-09-12 08:26:40 +0300

    task: added function that checks password validity

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -8,3 +8,8 @@ def hash_password(password: str) -> bytes:
     """Hashes a pwd"""
     salt = bcrypt.gensalt()
     return bcrypt.hashpw(str.encode(password), salt)
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """checks if pwd is valid"""
+    return bcrypt.checkpw(str.encode(password), hashed_password)
