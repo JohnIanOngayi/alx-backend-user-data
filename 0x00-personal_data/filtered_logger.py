395e56e662e3dfd1af0fc1f010872ba833fab3b7
JohnIanOngayi <ongayijohnian@gmail.com>
2024-09-12 07:34:59 +0300

    task: added get_db function

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -3,9 +3,55 @@
 """Module defines function that obufscates sensitive info"""
 from typing import List
 import re
+import os
 import logging
+from mysql.connector import MySQLConnection
 
 PII_FIELDS = ("name", "email", "phone", "ssn", "password")
+
+
+def filter_datum(
+    fields: List[str], redaction: str, message: str, separator: str
+) -> str:
+    """
+    returns obfuscated message
+
+    args:
+        fields (List[str]): fields to be obfuscated
+        redaction (str): obfsuscation string
+        message (str): string to be obfuscated
+        separator (str): character that separates fields in message
+
+    returns:
+        str : obfuscated message
+    """
+    pattern = "|".join([f"{field}=[^\\{separator}]*" for field in fields])
+    return re.sub(pattern,
+                  lambda m: f"{m.group(0).split('=')[0]}={redaction}",
+                  message)
+
+
+def get_logger() -> logging.Logger:
+    """returns a logger object"""
+    user_data = logging.Logger("user_data", level=logging.INFO)
+    formatter = RedactingFormatter(list(PII_FIELDS))
+    stream_handler = logging.StreamHandler()
+    stream_handler.setFormatter(formatter)
+    user_data.addHandler(stream_handler)
+    user_data.propagate = False
+
+    return user_data
+
+
+def get_db() -> MySQLConnection:
+    """Returns a MySQLConnection object to connect to the database"""
+    username = os.getenv("PERSONAL_DATA_DB_USERNAME")
+    password = os.getenv("PERSONAL_DATA_DB_PASSWORD")
+    db_host = os.getenv("PERSONAL_DATA_DB_HOST", "localhost")
+    db_name = os.getenv("PERSONAL_DATA_DB_NAME")
+    return MySQLConnection(
+        user=username, password=password, host=db_host, database=db_name
+    )
 
 
 class RedactingFormatter(logging.Formatter):
@@ -27,34 +73,3 @@ class RedactingFormatter(logging.Formatter):
             separator=self.SEPARATOR,
         )
         return super(RedactingFormatter, self).format(record=record)
-
-
-def filter_datum(
-    fields: List[str], redaction: str, message: str, separator: str
-) -> str:
-    """
-    returns obfuscated message
-
-    args:
-        fields (List[str]): fields to be obfuscated
-        redaction (str): obfsuscation string
-        message (str): string to be obfuscated
-        separator (str): character that separates fields in message
-
-    returns:
-        str : obfuscated message
-    """
-    pattern = "|".join([f"{field}=[^\\{separator}]*" for field in fields])
-    return re.sub(pattern, lambda m: f"{m.group(0).split('=')[0]}={redaction}", message)
-
-
-def get_logger() -> logging.Logger:
-    """returns a logger object"""
-    user_data = logging.Logger("user_data", level=logging.INFO)
-    formatter = RedactingFormatter(list(PII_FIELDS))
-    stream_handler = logging.StreamHandler()
-    stream_handler.setFormatter(formatter)
-    user_data.addHandler(stream_handler)
-    user_data.propagate = False
-
-    return user_data
