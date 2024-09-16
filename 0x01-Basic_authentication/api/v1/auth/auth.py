3b8f7222b7674df8683dd4e1b8aac084835c44dc
JohnIanOngayi <ongayijohnian@gmail.com>
2024-09-16 19:36:02 +0300

    task: modified auth for task 3

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -7,55 +7,17 @@ from typing import List, Optional, TypeVar, Union
 
 
 # Task 3 Class
-# class Auth:
-#     """Authentication Class"""
-#
-#     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
-#         """function"""
-#         return False
-#
-#     def authorization_header(self, request=None) -> str:
-#         """function"""
-#         return None
-#
-#     def current_user(self, request=None) -> TypeVar("User"):
-#         """function"""
-#         return None
-
-
 class Auth:
     """Authentication Class"""
 
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
-        """returns if path requires authentication"""
-        if path and not path.endswith("/"):
-            path += "/"
-        if (
-            path is None
-            or excluded_paths is None
-            or len(excluded_paths) == 0
-            or path not in excluded_paths
-        ):
-            return True
+        """function"""
         return False
 
-    def authorization_header(
-        self, request: Optional[flask.Request]
-    ) -> Union[str, None]:
-        """
-        validates all user requests to secure the API
+    def authorization_header(self, request=None) -> Union[str, None]:
+        """function"""
+        return None
 
-        args:
-            request (flask.request) the request
-
-        returns:
-            None if Authorization header is non-existent
-            else value of Authorization header
-        """
-        if request is None:
-            return None
-        return request.headers.get("Authorization")
-
-    def current_user(self, request: Optional[flask.Request]) -> TypeVar("User"):
+    def current_user(self, request=None) -> TypeVar("User"):
         """function"""
         return None
