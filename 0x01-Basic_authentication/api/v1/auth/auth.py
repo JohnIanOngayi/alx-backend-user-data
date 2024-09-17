72ae01deced1b2d4edd3341124b8ac227616af96
JohnIanOngayi <ongayijohnian@gmail.com>
2024-09-17 05:44:12 +0300

    task: added wildcard handling for excluded_paths

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -11,16 +11,19 @@ class Auth:
 
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """returns if path requires authentication"""
-        if path and not path.endswith("/"):
-            path += "/"
-        if (
-            path is None
-            or excluded_paths is None
-            or len(excluded_paths) == 0
-            or path not in excluded_paths
-        ):
+        if path is None or excluded_paths is None or len(excluded_paths) == 0:
             return True
-        return False
+        if path.endswith("/"):
+            path = path[:-1]
+        for excluded_path in excluded_paths:
+            if excluded_path.endswith("/"):
+                excluded_path = excluded_path[:-1]
+            if excluded_path.endswith("*"):
+                if path.startswith(excluded_path[:-1]):
+                    return False
+            if path == excluded_path:
+                return False
+        return True
 
     def authorization_header(self, request):
         """
